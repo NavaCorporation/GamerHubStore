8f7c246fe32ed22c7b3c4db479577ea2248734ab
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-05-26 11:11:12 -0500

    Creacion de modulos

---
+++ GamerHub_Store/src/app/modules/shopping/components/purchase-request/purchase-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './purchase-request.component.html',
+  styleUrl: './purchase-request.component.css'
+})
+export class PurchaseRequestComponent {
+
+}

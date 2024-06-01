c18c8d75f54edf13fc1fd8d109c2af29a716946e
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-06-01 11:54:31 -0500

    Login sin fondo

--- GamerHub_Store/src/app/app.component.ts
+++ GamerHub_Store/src/app/app.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./modules/shared/components/header/header.component";
 import { FooterComponent } from "./modules/shared/components/footer/footer.component";
-
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatCardModule} from '@angular/material/card';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, MatCardModule, MatDividerModule, MatButtonModule, MatProgressBarModule]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'GameHub_Store';
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
+
+  constructor(private router: Router) {}
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
 }
--- GamerHub_Store/src/app/app.routes.ts
+++ GamerHub_Store/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/security/components/login/login.component';
+import { ClientComponent } from './modules/clients/components/client/client.component';
 
 
 export const routes: Routes = [
-    //{ path: 'login', component: 'loginComponent' },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'client', component: ClientComponent },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];

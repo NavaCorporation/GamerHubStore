5a9e0e128d89812f4776988400364f16f99511cf
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-06-06 08:27:56 -0500

    Alpha

--- GamerHub_Store/src/app/modules/security/services/authentication.service.ts
+++ GamerHub_Store/src/app/modules/security/services/authentication.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  
+  private apiUrl = 'http://localhost:8080/api/auth'; // Aun me falta la url
+
+  constructor(private http: HttpClient) { }
+//corregir luego cuando este el backend
+  login(credentials: {email: string, password: string}): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
 }

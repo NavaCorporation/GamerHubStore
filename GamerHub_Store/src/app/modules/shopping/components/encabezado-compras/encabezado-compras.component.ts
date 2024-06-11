50097a273e7efe6fc54d6f0fd413d806e39fdc7d
giazr <gia.zhunior@ug.edu.ec>
2024-06-11 11:29:55 -0500

    Add encabezado_compras

---
+++ GamerHub_Store/src/app/modules/shopping/components/encabezado-compras/encabezado-compras.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encabezado-compras',
+  standalone: true,
+  imports: [],
+  templateUrl: './encabezado-compras.component.html',
+  styleUrl: './encabezado-compras.component.css'
+})
+export class EncabezadoComprasComponent {
+
+}

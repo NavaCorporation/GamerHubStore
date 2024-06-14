a7a48daa2318736378221f912c78d6c11aba9aae
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-06-14 00:01:25 -0500

    Notificaciones

--- GamerHub_Store/src/app/modules/security/components/security-notifications/security-notifications.component.ts
+++ GamerHub_Store/src/app/modules/security/components/security-notifications/security-notifications.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { NotificationService } from '../../services/notifications/notification.service';
+
 
 @Component({
   selector: 'app-security-notifications',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, RouterLink ],
   templateUrl: './security-notifications.component.html',
   styleUrl: './security-notifications.component.css'
 })
-export class SecurityNotificationsComponent {
+export class SecurityNotificationsComponent implements OnInit {
 
+  notifications: { type : string, message: string, read: boolean }[] = [];
+  showNotifications: boolean = false;
+
+  constructor( private ntserv: NotificationService) {}
+
+  ngOnInit(): void {
+    this.notifications = this.ntserv.getNotifications();
+  }
+
+  toggleNotifications(): void {
+    this.showNotifications = !this.showNotifications;
+  }
+  get unreadNotificationsCount(): number {
+    return this.notifications.filter(notification => !notification.read).length;
+  }
+
+  markAsRead(index: number): void {
+    if (index >= 0 && index < this.notifications.length) {
+      this.ntserv.markAsRead(index);
+    }
+  }
 }
--- GamerHub_Store/src/app/modules/shopping/components/encabezado-compras/encabezado-compras.component.ts
+++ GamerHub_Store/src/app/modules/shopping/components/encabezado-compras/encabezado-compras.component.ts
@@ -3,16 +3,18 @@ import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthenticationService } from '../../../security/services/authService/authentication.service';
 import { DatosUser } from '../../../security/models/datosUser';
+import { SecurityNotificationsComponent } from "../../../security/components/security-notifications/security-notifications.component";
 
 @Component({
-  selector: 'app-encabezado-compras',
-  standalone: true,
-  imports: [ CommonModule, RouterLink], 
-  templateUrl: './encabezado-compras.component.html',
-  styleUrl: './encabezado-compras.component.css'
+    selector: 'app-encabezado-compras',
+    standalone: true,
+    templateUrl: './encabezado-compras.component.html',
+    styleUrl: './encabezado-compras.component.css',
+    imports: [CommonModule, RouterLink, SecurityNotificationsComponent]
 })
 export class EncabezadoComprasComponent implements OnInit {
   currentUser: DatosUser | null = null;
+  notifications: [{ type: string, message: string }] = [{ type: 'Error', message: 'Intento fallido a la cuenta' }];
 
   constructor(private authService: AuthenticationService, private router: Router) {
     this.authService.currentUser.subscribe(user => this.currentUser = user);
@@ -32,4 +34,6 @@ export class EncabezadoComprasComponent implements OnInit {
   userEdit(): void {
     this.router.navigate(['/client']);
   }
+
+
 }

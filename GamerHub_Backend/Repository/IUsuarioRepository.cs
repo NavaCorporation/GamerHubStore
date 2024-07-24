d987a4453cb252b0b32a2ef00aecf140b0d6a5e4
Juan Vasconez <juanvasconez1905@gmail.com>
2024-07-24 08:39:18 -0500

    avance 2

--- GamerHub_Backend/Repository/IUsuarioRepository.cs
+++ GamerHub_Backend/Repository/IUsuarioRepository.cs
@@ -7,6 +7,8 @@ namespace GamerHub_Backend.Repository
         Task<Usuario?> GetByEmailAsync(string email);
         Task<Usuario?> LlamarPorNombre(string nombreUsuario);
         Task<Usuario?> ObtenerPorId(int id);
+        //Task<Usuario?> ModificarUsuario
+        //Task<Usuario?>ModificarFoto
         Task<int?> Crear(Usuario usuario);
         Task<Rol?> VerificarRol(int rolId);
         Task AddAsync(Usuario usuario);

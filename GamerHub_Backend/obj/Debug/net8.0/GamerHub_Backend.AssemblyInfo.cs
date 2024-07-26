1b172d26ed09fbbe588c160a98dd7ab528c309ee
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-07-26 13:56:46 -0500

    modified:   GamerHub_Backend/.vs/GamerHub_Backend/DesignTimeBuild/.dtbcache.v2 	modified:   GamerHub_Backend/.vs/GamerHub_Backend/v17/.futdcache.v2 	modified:   GamerHub_Backend/.vs/GamerHub_Backend/v17/.suo 	modified:   GamerHub_Backend/.vs/GamerHub_Backend/v17/DocumentLayout.json 	modified:   GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.AssemblyInfo.cs modified:   GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.AssemblyInfoInputs.cache 	new file:   GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.csproj.AssemblyReference.cache 	modified:   GamerHub_Backend/obj/GamerHub_Backend.csproj.nuget.dgspec.json 	modified:   GamerHub_Backend/obj/GamerHub_Backend.csproj.nuget.g.props 	modified:   GamerHub_Backend/obj/project.assets.json 	modified:   GamerHub_Backend/obj/project.nuget.cache

--- GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.AssemblyInfo.cs
+++ GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamerHub_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0fb83a0f4d73d00ebf5581ebb3df46b07dfc07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfbbdb434b835b936320386ae31ea6c15c9d76f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamerHub_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamerHub_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

86b13dfb405c3f9766b742c71088e29eb960dc54
Juan Vasconez <juanvasconez1905@gmail.com>
2024-07-30 07:52:44 -0500

    aa
    aa

--- GamerHub_Store/src/app/modules/administration/components/services/user.service.ts
+++ GamerHub_Store/src/app/modules/administration/components/services/user.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
 
+export class UserService {
+private apiUrl = 'http://localhost:28850'
+constructor(private http: HttpClient) { }
   private userSubject = new BehaviorSubject<any>({
     name: localStorage.getItem('userName') || 'Nombre del Usuario',
     email: localStorage.getItem('userEmail') || 'Email del Usuario',
@@ -18,5 +22,16 @@ export class UserService {
     localStorage.setItem('userName', user.name);
     localStorage.setItem('userEmail', user.email);
     localStorage.setItem('userPhotoUrl', user.photoUrl);
+    
+  }
+  modificarUsuario(usuario: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/update`, usuario);
+  }
+
+  modificarFoto(id: number, profilePicture: File): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append('profilePicture', profilePicture);
+    
+    return this.http.put(`${this.apiUrl}/update-photo/${id}`, formData);
   }
 }

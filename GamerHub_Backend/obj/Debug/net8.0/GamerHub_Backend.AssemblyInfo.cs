786714825b1dd320eac291ebd4ed98bdace6087e
giazr <gia.zhunior@ug.edu.ec>
2024-08-04 19:00:25 -0500

    prueba

--- GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.AssemblyInfo.cs
+++ GamerHub_Backend/obj/Debug/net8.0/GamerHub_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamerHub_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2931addb3893db3637ff0ba59d76f92900149e1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b9cbac19dbd2393d8617732aa87c4a2c4b790e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamerHub_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamerHub_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

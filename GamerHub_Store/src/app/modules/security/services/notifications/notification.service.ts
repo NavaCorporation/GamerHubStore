fb28b4bdaecf2ea9fa5ebe383fa4d91601bec74a
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-08-08 23:05:07 -0500

    as

--- GamerHub_Store/src/app/modules/security/services/notifications/notification.service.ts
+++ GamerHub_Store/src/app/modules/security/services/notifications/notification.service.ts
@@ -46,7 +46,12 @@ export class NotificationService {
     const offers = [
       { message: '¡50% de descuento en todos los accesorios para juegos!', type: 'Oferta' },
       { message: '¡Compre 1 y obtenga 1 gratis en juegos seleccionados', type: 'Oferta' },
-      { message: '¡Descuentos exclusivos para pedidos anticipados disponibles ahora!', type: 'Oferta' }
+      { message: '¡Descuentos exclusivos para pedidos anticipados disponibles ahora!', type: 'Oferta' },
+      { message: '¡Reserva tu juego favorito ahora y recibe contenido exclusivo!', type: 'Oferta' },
+      { message: '¡Ahorra un 40% en todos los títulos de nuestra selección especial!', type: 'Oferta' },
+      { message: '¡Obtén un cupón de $10 en tu próxima compra al gastar $50 o más!', type: 'Oferta' },
+      { message: '¡Descuentos increíbles en consolas de última generación!', type: 'Oferta' },
+      { message: '¡No te pierdas nuestras ofertas relámpago de medianoche!', type: 'Oferta' },
     ];
 
     offers.forEach(offer => this.addNotification(offer));
